0ba0ed844255c26321ede31b50f6e6d58aa92491
shengyao1998 <105561604+shengyao1998@users.noreply.github.com>
2024-02-20 09:02:28 +0000

    Scientific Computing with Python Projects 4: Polygon Area Calculator

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,52 @@
+############
+# Rectangle Class
+############
 class Rectangle:
+  def __init__ (self, width, height):
+    self.width = width
+    self.height = height
+
+  def __str__(self):
+    return f"Rectangle(width={self.width}, height={self.height})"
+    
+  def set_width (self, width):
+    self.width = width
+    
+  def set_height (self, height):
+    self.height = height
+    
+  def get_area (self):
+    return self.width * self.height 
+
+  def get_perimeter (self):
+    return (2 * self.width) + (2 * self.height)
+    
+  def get_diagonal (self):
+    return ((self.width ** 2) + (self.height ** 2)) ** 0.5
+    
+  def get_picture (self):
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    
+    pic = ""
+    for i in range(self.height):
+      pic += "*" * self.width + "\n"
+    return pic
+    
+  def get_amount_inside (self,rect):
+    return (self.width // rect.width) * (self.height // rect.height) 
 
 
 
+############
+# Square Class (Subclass of Rectangle))
+############
+class Square (Rectangle):
+  def __init__ (self, side):
+    self.width = self.height = side
+    
+  def __str__(self):
+    return f"Square(side={self.width})"
 
-class Square:
+  def set_side (self, side):
+    self.width = self.height = side
